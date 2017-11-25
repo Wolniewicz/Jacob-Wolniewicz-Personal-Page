b09815bb5f072471c86772aa5ffb0b441c687c7a
Jacob Wolniewicz <jacobwolniewicz@Jacobs-MacBook-Pro.local>
2017-11-25 10:14:59 -0700

    Adding file pathing

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 'use strict';
 var fs = require('fs');
 
-console.log("Here");
+console.log('Loading lambda function for jacob.wolniewicz.com.')
 
- exports.get = function(event, context) {
-   console.log(event);
-   console.log(context);
-   var contents = fs.readFileSync("public/index.html");
-   context.succeed({
-     statusCode: 200,
-     body: contents.toString(),
-     headers: {'Content-Type': 'text/html'}
-   });
- };
+// Primary get function for files
+exports.get = function(event, context) {
+  // Get the file name, defaulting to index.html if no file name is specified.
+  var filename = event.path;
+  if (filename == '/') {
+    filename = '/index.html'
+  }
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('File name = "' + filename + "'")
+
+  // Read the file from the file system.
+  // TODO: Handle file-not-found in a more intelligent way, returning 404.
+  var contents = fs.readFileSync("public" + filename);
+
+  // Return success with the contents of the file.
+  context.succeed({
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'Content-Type': 'text/html'}
+  });
+};
